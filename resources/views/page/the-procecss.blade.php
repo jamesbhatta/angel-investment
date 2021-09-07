727eab4ada1e94de8b584cb4587b69ffe47eab5f
manojbhatta <bob@example.com>
2021-09-07 12:01:12 +0545

    the process page

--- resources/views/page/the-procecss.blade.php
+++ resources/views/page/the-procecss.blade.php
@@ -1,5 +1,169 @@
 @extends('layouts.app')
 
 @section('content')
-    
+     <!-- Main Content -->
+     <section class="main process-banner ">
+        <!-- ======= Breadcrumbs ======= -->
+        <section id="breadcrumbs" class="breadcrumbs">
+            <div class="container">
+
+                <ol>
+                    <li><a href="index.html">Home</a></li>
+                    <li>Process</li>
+                </ol>
+                <h2>Process</h2>
+
+            </div>
+        </section><!-- End Breadcrumbs -->
+        <section class="process">
+            <div class="section-title ">
+                <h2>For Investor:</h2>
+            </div>
+
+            <div class="investor">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-md-12">
+                            <div class="timeline">
+                                <ul>
+
+
+
+                                    <li>
+                                        <div data-aos="zoom-out-right">
+                                            <div class="timeline-content">
+
+                                                <p>1) Register your details with our platform.</p>
+                                            </div>
+                                        </div>
+
+                                    </li>
+                                    <li>
+                                        <div data-aos="zoom-out-left">
+                                            <div class="timeline-content">
+                                                <p>2) Browse through the selection of projects.</p>
+                                            </div>
+                                        </div>
+
+                                    </li>
+                                    <li>
+                                        <div data-aos="zoom-out-right">
+                                            <div class="timeline-content">
+
+                                                <p>3) Shortlist the projects that have captured your interest.</p>
+                                            </div>
+                                        </div>
+
+                                    </li>
+                                    <li>
+                                        <div data-aos="zoom-out-left">
+
+                                            <div class="timeline-content">
+
+                                                <p>4) The Mediation Process begins.</p>
+                                            </div>
+                                        </div>
+
+                                    </li>
+                                    <li>
+                                        <div data-aos="zoom-out-right">
+                                            <div class="timeline-content">
+
+                                                <p>5) Close the deal!</p>
+                                            </div>
+                                        </div>
+
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <h4 class=" text-center mt-5 mb-5" style="color: #018ee3; font-size: 24px; font-weight: 500;">Another
+                Success story begins...</h4>
+
+            <!-- joinNow buttton -->
+            <button class="getStart d-flex justify-content-center m-auto mb-4">Join us now</button>
+            <hr>
+
+            <div class="section-title ">
+                <h2>For Entrepreneur:</h2>
+            </div>
+
+
+
+            <div class="enterpernur">
+
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-md-12">
+                            <div class="timeline">
+                                <ul>
+
+
+
+                                    <li>
+                                        <div data-aos="zoom-out-right">
+                                            <div class="timeline-content">
+
+                                                <p>1) Register your details with our platform.</p>
+                                            </div>
+                                        </div>
+
+                                    </li>
+                                    <li>
+                                        <div data-aos="zoom-out-left">
+                                            <div class="timeline-content">
+                                                <p>2) Upload your pitch on the platform.</p>
+                                            </div>
+                                        </div>
+
+                                    </li>
+                                    <li>
+                                        <div data-aos="zoom-out-right">
+                                            <div class="timeline-content">
+
+                                                <p>3) Get your project connected with the interested investors.</p>
+                                            </div>
+                                        </div>
+
+                                    </li>
+                                    <li>
+                                        <div data-aos="zoom-out-left">
+
+                                            <div class="timeline-content">
+
+                                                <p>4) Mediation process starts.</p>
+                                            </div>
+                                        </div>
+
+                                    </li>
+                                    <li>
+                                        <div data-aos="zoom-out-right">
+                                            <div class="timeline-content">
+
+                                                <p>5) Close the deal!</p>
+                                            </div>
+                                        </div>
+
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <h4 class=" text-center mt-5 mb-5" style="color: #018ee3; font-size: 24px; font-weight: 500;">
+                Another Idea realised into the world...</h4>
+
+                
+            <!-- joinNow buttton -->
+            <button class="getStart d-flex justify-content-center m-auto mb-4">Join us now</button>
+        </section>
+    </section>
+
+    <!-- Main Content-end -->
 @endsection
